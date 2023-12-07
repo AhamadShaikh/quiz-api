a87a766b5d6e0015a2ad9b6601d36fc3416c0454
AhamadShaikh <shaikhahamadrasul786@gmail.com>
2023-12-07 09:21:27 +0530

    server

--- model/formModel.js
+++ model/formModel.js
@@ -1,40 +1,35 @@
 const { default: mongoose } = require("mongoose");
 
 const formEditSchema = new mongoose.Schema({
-    "categorize": [
+    categorize: [
         {
-            "categorize_points": Number,
-            "categorize_description": String,
-            "categorize_media": String,
-            "category": [String],
-            "itemsAndBelongsTo": [
-                {
-                    "[item_content]": "[belongsTo_content]"
-                }
-            ]
+            categorize_points: { type: Number, required: true },
+            categorize_description: { type: String, required: true },
+            categorize_media: { type: String, required: true },
+            category: [{ type: String, required: true }],
+            itemsAndBelongsTo: [{ type: Object, required: true }]
         }
     ],
-    "cloze": [
+    cloze: [
         {
-            "cloze_points": Number,
-            "cloze_preview": String,
-            "cloze_question": String,
-            "cloze_media": String,
-            "cloze_options": [String]
+            cloze_points: { type: Number, required: true },
+            cloze_preview: { type: String, required: true },
+            cloze_question: { type: String, required: true },
+            cloze_media: { type: String, required: true },
+            cloze_options: [{ type: String, required: true }]
         }
     ],
-    "comprehension": [
+    comprehension: [
         {
-            "comprehension_points": Number,
-            "comprehension_description": String,
-            "comprehension_passage": String,
-            "comprehension_media": String,
-            "comprehension_questions": String,
-            "comprehensionOptions": [String]
+            comprehension_points: { type: Number, required: true },
+            comprehension_description: { type: String, required: true },
+            comprehension_passage: { type: String, required: true },
+            comprehension_media: { type: String, required: true },
+            comprehension_questions: { type: String, required: true },
+            comprehension_options: [{ type: String, required: true }],
         }
     ]
-}
-)
+})
 
 const Form = mongoose.model("formdata", formEditSchema)
 
