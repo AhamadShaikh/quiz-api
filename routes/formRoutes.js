212578fda2849925b0fea6a4f582f17a4f4fc1ea
AhamadShaikh <shaikhahamadrasul786@gmail.com>
2023-12-07 11:23:11 +0530

    server ready

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -6,19 +6,22 @@ const router = express.Router()
 
 router.post("/create", async (req, res) => {
     try {
-        const formCreate = await Form.create({ ...req.body })
-        res.status("200").json({ msg: "form creared successfully" })
+        console.log(req.body)
+        const formCreate = await Form.create(req.body)
+        console.log(formCreate)
+        res.status(200).json({ msg: "form creared successfully" })
     } catch (error) {
-        res.status("400").json({ msg: "Something went wrong" })
+        res.status(400).json({ msg: "Something went wrong" })
     }
 })
 
 router.get("/formdata", async (req, res) => {
     try {
         const formData = await Form.find({})
-        res.status("200").json({ data: formData })
+        console.log(formData)
+        res.status(200).json({formData })
     } catch (error) {
-        res.status("400").json({ msg: "Something went wrong" })
+        res.status(400).json({ msg: "Something went wrong" })
     }
 })
 
