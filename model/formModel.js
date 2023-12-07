bd1d99ae6a86f8eb9e368f2448795fed9c0fbf61
AhamadShaikh <shaikhahamadrasul786@gmail.com>
2023-12-07 09:29:01 +0530

    done

--- model/formModel.js
+++ model/formModel.js
@@ -1,36 +1,41 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+
+const itemsAndBelongsToSchema = new mongoose.Schema({
+    key: { type: String, required: true },
+    value: { type: String, required: true }
+});
+
+const categorizeSchema = new mongoose.Schema({
+    categorize_points: { type: Number, required: true },
+    categorize_description: { type: String, required: true },
+    categorize_media: { type: String, default: "none" },
+    category: [{ type: String, required: true }],
+    itemsAndBelongsTo: [itemsAndBelongsToSchema]
+});
+
+const clozeSchema = new mongoose.Schema({
+    cloze_points: { type: Number, required: true },
+    cloze_preview: { type: String, default: "none" },
+    cloze_question: { type: String, required: true },
+    cloze_media: { type: String, default: "none" },
+    cloze_options: [{ type: String, required: true }]
+});
+
+const comprehensionSchema = new mongoose.Schema({
+    comprehension_points: { type: Number, default: null },
+    comprehension_description: { type: String, required: true },
+    comprehension_passage: { type: String, required: true },
+    comprehension_media: { type: String, default: "none" },
+    comprehension_questions: { type: String, required: true },
+    comprehension_options: [{ type: String, required: true }]
+});
 
 const formEditSchema = new mongoose.Schema({
-    categorize: [
-        {
-            categorize_points: { type: Number, required: true },
-            categorize_description: { type: String, required: true },
-            categorize_media: { type: String, required: true },
-            category: [{ type: String, required: true }],
-            itemsAndBelongsTo: [{ type: Object, required: true }]
-        }
-    ],
-    cloze: [
-        {
-            cloze_points: { type: Number, required: true },
-            cloze_preview: { type: String, required: true },
-            cloze_question: { type: String, required: true },
-            cloze_media: { type: String, required: true },
-            cloze_options: [{ type: String, required: true }]
-        }
-    ],
-    comprehension: [
-        {
-            comprehension_points: { type: Number, required: true },
-            comprehension_description: { type: String, required: true },
-            comprehension_passage: { type: String, required: true },
-            comprehension_media: { type: String, required: true },
-            comprehension_questions: { type: String, required: true },
-            comprehension_options: [{ type: String, required: true }],
-        }
-    ]
-})
+    categorize: [categorizeSchema],
+    cloze: [clozeSchema],
+    comprehension: [comprehensionSchema]
+});
 
-const Form = mongoose.model("formdata", formEditSchema)
+const Form = mongoose.model("FormData", formEditSchema);
 
-module.exports = Form 
+module.exports = Form;
